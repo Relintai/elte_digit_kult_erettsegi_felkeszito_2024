62b8af52b500631426f3ca7e4a72ed38fb7377ac
Relintai <relintai@protonmail.com>
2024-02-04 19:29:49 +0100

    Added missing files.

---
+++ VII. Feladatcsoport - Programozás/PO05 – Fuvarozás/Megoldás/C#/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PO5
+{
+    internal class Program
+    {
+        private class Fuvar
+        {
+            public string Megnevezes { get; set; }
+            public int Tavolsag { get; set; }
+            public double Uzemanyag { get; set; }
+            public int Suly { get; set; }
+            public int Honap { get; set; }
+            public int Nap { get; set; }
+
+            public double Fogyasztas { get { return (Uzemanyag / Tavolsag) * 100.0; } }
+
+            public Fuvar(string megnevezes, int tavolsag, double uzemanyag, int suly, int honap, int nap)
+            {
+                Megnevezes = megnevezes;
+                Tavolsag = tavolsag;
+                Uzemanyag = uzemanyag;
+                Suly = suly;
+                Honap = honap;
+                Nap = nap;
+            }
+        }
+
+        private static Fuvar[] fuvarok;
+
+
+        static void Main(string[] args)
+        {
+            string[] adatok = File.ReadAllLines(@"D:\Egyetem\Kiadvány\05_Fuvar\Adatok.csv");
+            fuvarok = new Fuvar[adatok.Length];
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                string[] adatsor = adatok[i].Split(';');
+                fuvarok[i] = new Fuvar(adatsor[0], Convert.ToInt32(adatsor[1]), Convert.ToDouble(adatsor[2].Replace('.',',')), Convert.ToInt32(adatsor[3]), Convert.ToInt32(adatsor[4]), Convert.ToInt32(adatsor[5]));
+            }
+
+            int maximumIndex = 0;
+            for (int i = 1; i < fuvarok.Length; i++)
+            {
+                if (fuvarok[i].Fogyasztas > fuvarok[maximumIndex].Fogyasztas)
+                {
+                    maximumIndex = i;
+                }
+            }
+            Console.WriteLine($"A legtöbbet fogasztott jármű neve: {fuvarok[maximumIndex].Megnevezes}, fogyasztása: {fuvarok[maximumIndex].Fogyasztas}, ekkor {fuvarok[maximumIndex].Suly} kg súlyt szálított.");
+
+            List<string> osszesNev = new List<string>();
+            foreach (Fuvar f in fuvarok)
+            {
+                if (!osszesNev.Contains(f.Megnevezes)) { osszesNev.Add(f.Megnevezes); }
+            }
+            string[] export = new string[osszesNev.Count + 1];
+            export[0] = DateTime.Now.ToShortDateString();
+            for (int i = 0; i < osszesNev.Count; i++)
+            {
+                export[i + 1] = $"{osszesNev[i]}: {adatokJarmurol(osszesNev[i])}";
+            }
+            File.WriteAllLines("export.txt", export);
+
+            Console.WriteLine(Legtobb(5,17,7,14));
+            Nagyobb(200);
+            Console.WriteLine(Osszes("Teher 2.", 5,17,7,21));
+            Magas(15.1);
+
+            Console.ReadKey();
+        }
+
+        private static string adatokJarmurol(string nev)
+        {
+            int bejegyzesek = 0;
+            int tavolsagOsszeg = 0;
+            double fogyasztasOsszeg = 0.0;
+            int sulyOsszeg = 0;
+
+            foreach (Fuvar f in fuvarok)
+            {
+                if (f.Megnevezes == nev)
+                {
+                    bejegyzesek++;
+                    tavolsagOsszeg += f.Tavolsag;
+                    fogyasztasOsszeg += f.Fogyasztas;
+                    sulyOsszeg += f.Suly;
+                }
+            }
+
+            return $"Teljes megtett távolság: {tavolsagOsszeg} km, Átlagfogyasztás: {fogyasztasOsszeg / bejegyzesek} l/100 km, Összesített súly: {sulyOsszeg} kg";
+        }
+
+        private static string Legtobb(int honaptol, int naptol, int honapig, int napig)
+        {
+            Dictionary<string, int> nevFuvar = new Dictionary<string, int>();
+ 
+            foreach (Fuvar f in fuvarok)
+            {
+                if (f.Honap >= honaptol && f.Honap <= honapig && (f.Honap != honaptol || f.Nap >= naptol) && (f.Honap != honapig || f.Nap <= napig))
+                {
+                    if (!nevFuvar.ContainsKey(f.Megnevezes))
+                    {
+                        nevFuvar.Add(f.Megnevezes, 1);
+                    }
+                    else 
+                    {
+                        nevFuvar[f.Megnevezes]++;
+                    }
+                }
+            }
+
+            string vissza = "Nem történt fuvar";
+            if (nevFuvar.Keys.Count > 0)
+            {
+                int maximum = 0;
+                foreach (string i in nevFuvar.Keys)
+                {
+                    if (nevFuvar[i] > maximum) 
+                    {
+                        maximum = nevFuvar[i];
+                        vissza = i;
+                    }
+                }
+            }
+
+            return vissza;
+        }
+
+        private static void Nagyobb(int suly)
+        {
+            foreach (Fuvar f in fuvarok)
+            {
+                if (f.Suly > suly)
+                {
+                    Console.WriteLine($"{f.Honap.ToString("00")}.{f.Nap.ToString("00")} {f.Megnevezes}");
+                }
+            }
+        }
+
+        private static int Osszes(string nev, int honaptol, int naptol, int honapig, int napig)
+        {
+            int tavolsag = 0;
+
+            foreach (Fuvar f in fuvarok)
+            {
+                if (f.Megnevezes == nev && f.Honap >= honaptol && f.Honap <= honapig && (f.Honap != honaptol || f.Nap >= naptol) && (f.Honap != honapig || f.Nap <= napig))
+                {
+                    tavolsag += f.Tavolsag;
+                }
+            }
+
+            return tavolsag;
+        }
+
+        private static void Magas(double fogyasztas)
+        {
+            List<string> kiirtak = new List<string>();
+            foreach (Fuvar f in fuvarok)
+            {
+                if (f.Fogyasztas > fogyasztas)
+                {
+                    if (!kiirtak.Contains(f.Megnevezes))
+                    {
+                        Console.WriteLine(f.Megnevezes);
+                        kiirtak.Add(f.Megnevezes);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ VII. Feladatcsoport - Programozás/PO05 – Fuvarozás/Megoldás/Python/PO05.py
@@ -0,0 +1,111 @@
+
+from datetime import datetime
+from tkinter import N
+
+class Fuvar:
+    def __init__(self, Megnevezes, Tavolsag, Uzemanyag, Suly, Honap, Nap):
+        self.Megnevezes = Megnevezes
+        self.Tavolsag = Tavolsag
+        self.Uzemanyag = Uzemanyag
+        self.Suly = Suly
+        self.Honap = Honap
+        self.Nap = Nap
+    
+    @property
+    def fogyasztas(self):
+        return (self.Uzemanyag / self.Tavolsag)*100.0
+
+fuvarok = []
+f = open("Adatok.csv", "r")
+for tSor in f:
+    sor = tSor.split(';')
+    fuvarok.append(Fuvar(sor[0],int(sor[1]),float(sor[2]),int(sor[3]),int(sor[4]),int(sor[5])))
+f.close()
+
+def AdatokJarmurol(nev):
+    bejegyzesek = 0
+    tavolsagOsszeg = 0
+    fogyasztasOsszeg = 0.0
+    sulyOsszeg = 0
+
+    for f in fuvarok:
+        if f.Megnevezes == nev:
+            bejegyzesek+=1
+            tavolsagOsszeg += f.Tavolsag
+            fogyasztasOsszeg += f.fogyasztas
+            sulyOsszeg += f.Suly
+
+    return "Teljes megtett tavolsag: " + str(tavolsagOsszeg) + " km, Atlagfogyasztas: " + str(fogyasztasOsszeg / bejegyzesek) + " l/100 km, Osszesitett suly:  " + str(sulyOsszeg) + " kg"
+
+def Legtobb(honaptol,naptol, honapig, napig):
+    nevFuvar = dict()
+
+    for f in fuvarok:
+        if f.Honap >= honaptol and f.Honap <= honapig and (f.Honap != honaptol or f.Nap >= naptol) and (f.Honap != honapig or f.Nap <= napig):
+            if f.Megnevezes not in nevFuvar.keys():
+                nevFuvar[f.Megnevezes] = 1
+            else:
+                nevFuvar[f.Megnevezes] += 1
+
+    vissza = "Nem tortent fuvar"
+    if len(nevFuvar.keys()) > 0:
+        maximum=0
+        for i in nevFuvar.keys():
+            if nevFuvar[i] > maximum:
+                maximum = nevFuvar[i]
+                vissza = i
+
+    return vissza
+
+def Nagyobb(suly):
+    for f in fuvarok:
+        if f.Suly > suly:
+            print(str(f.Honap) + "." + str(f.Nap) + " " + f.Megnevezes)
+
+
+def Osszes(nev, honaptol,naptol, honapig, napig):
+    tavolsag = 0
+
+    for f in fuvarok:
+        if f.Megnevezes == nev and f.Honap >= honaptol and f.Honap <= honapig and (f.Honap != honaptol or f.Nap >= naptol) and (f.Honap != honapig or f.Nap <= napig):
+            tavolsag += f.Tavolsag
+
+    return tavolsag
+
+def Magas(fogyasztas):
+    kiirtak = []
+
+    for f in fuvarok:
+        if f.fogyasztas > fogyasztas:
+            if f.Megnevezes not in kiirtak:
+                print(f.Megnevezes)
+                kiirtak.append(f.Megnevezes)
+
+
+maximumIndex=0
+for i in range(1,len(fuvarok)):
+    #print(fuvarok[i].fogyasztas)
+    if fuvarok[maximumIndex].fogyasztas < fuvarok[i].fogyasztas:
+        maximumIndex = i
+
+print("A legtobbet fogyasztott jarmu neve: " + fuvarok[maximumIndex].Megnevezes + ", fogyasztasa: " + str(fuvarok[maximumIndex].fogyasztas) + ", ekkor " + str(fuvarok[maximumIndex].Suly) + " kg sulyt szalitott." )
+
+
+osszesNev = []
+for f in fuvarok:
+    if f.Megnevezes not in osszesNev:
+        osszesNev.append(f.Megnevezes)
+
+kimenet = []
+kimenet.append(str(datetime.today().year) + "." + str(datetime.today().month) + "." + str(datetime.today().day) + "\n")
+for n in osszesNev:
+    kimenet.append(n + ": " + AdatokJarmurol(n) + "\n")
+
+f = open("export.txt", "w")
+f.writelines(kimenet)
+f.close()
+
+print(Legtobb(5,17,7,14))
+Nagyobb(200)
+print(Osszes("Teher 2.", 5,17,7,21))
+Magas(15.1)
